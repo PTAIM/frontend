34231606d75a3f2a27fc3a4404b27c89a076840b
ThiagoORuby <trs@ic.ufal.br>
2025-10-27 19:57:49 -0300

    feat: Cria todos os services (comunicação com Back)  usados na plataforma

---
+++ app/services/pacientes.ts
@@ -0,0 +1,52 @@
+import { handleApiError } from "~/lib/api-error";
+import { api } from "~/lib/axios";
+import type { Message } from "~/types/common";
+import type {
+  CriarPaciente,
+  AtualizarPaciente,
+  PacientesParams,
+  PacienteDetalhes,
+  PacientesData,
+} from "~/types/paciente";
+
+class PacienteService {
+  async create(data: CriarPaciente) {
+    try {
+      const response = await api.post<Message>("/pacientes", data);
+      return response.data;
+    } catch (error) {
+      throw new Error(handleApiError(error));
+    }
+  }
+
+  async update(id: number, data: AtualizarPaciente) {
+    try {
+      const response = await api.put<Message>(`/pacientes/${id}`, data);
+      return response.data;
+    } catch (error) {
+      throw new Error(handleApiError(error));
+    }
+  }
+
+  async readAll(params: PacientesParams) {
+    try {
+      const response = await api.get<PacientesData>("/pacientes", {
+        params: params,
+      });
+      return response.data;
+    } catch (error) {
+      throw new Error(handleApiError(error));
+    }
+  }
+
+  async read(id: number) {
+    try {
+      const response = await api.get<PacienteDetalhes>(`/pacientes/${id}`);
+      return response.data;
+    } catch (error) {
+      throw new Error(handleApiError(error));
+    }
+  }
+}
+
+export const pacienteService = new PacienteService();
